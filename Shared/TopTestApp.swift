df123afb1bac6ca0728cf4eebf7ba3eb7dc1d01e
hugoshortcut <107543511+hugoshortcut@users.noreply.github.com>
2022-06-28 08:14:15 +0200

    Initial Commit

---
+++ Shared/TopTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopTestApp.swift
+//  Shared
+//
+//  Created by Hugo Diaz on 2022-06-28.
+//
+
+import SwiftUI
+
+@main
+struct TopTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
