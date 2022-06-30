d3256821ee3f0542aeb1a3ef78167324e2f029fc
hugoshortcut <107543511+hugoshortcut@users.noreply.github.com>
2022-06-30 10:18:51 +0200

    Moved DetailViewModel to its own file in View Models folder.

--- Shared/Scenes/Detail/DetailView.swift
+++ Shared/Scenes/Detail/DetailView.swift
@@ -7,31 +7,6 @@
 
 import SwiftUI
 
-class DetailViewModel: ObservableObject {
-    @Published var gifAddress: String = ""
-    @Published var gifTitle: String = ""
-
-    func fetchSpecificJSON() {
-        let whereLiveGifsAre = URL(string: current.realLiveJSONQueryLink)!
-
-        let welcomePublisher = URLSession.shared.dataTaskPublisher(for: whereLiveGifsAre)
-            .map(\.data)
-            .decode(type: Welcome.self, decoder: JSONDecoder())
-
-        welcomePublisher
-            .map { $0.data.first?.images.original.url ?? "" }
-            .replaceError(with: "Error Getting Gif Address")
-            .receive(on: RunLoop.main)
-            .assign(to: &$gifAddress)
-
-        welcomePublisher
-            .map { $0.data.first?.title ?? "Error Getting Gif Title" }
-            .replaceError(with: "")
-            .receive(on: RunLoop.main)
-            .assign(to: &$gifTitle)
-    }
-}
-
 struct DetailView: View {
     @EnvironmentObject var authViewModel: AuthorizeViewModel
 
